06c3562f668e724f68e37c1cb039a92645e63e3e
sofia <sdi1500051@di.uoa.gr>
2019-11-12 00:10:27 +0200

    new input, tuple.value->tuple.key + new value, list

--- inputFunctions.cpp
+++ inputFunctions.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <cstdlib>
+#include <stdio.h>
 #include<iostream>
 #include "inputFunctions.h"
 
@@ -8,10 +9,10 @@ using namespace std;
 int getArraySize(string filename,uint64_t & count){  //finds the size of array
     ifstream myReadFile;
     myReadFile.open(filename);
-    char output1[100],output2[100];
+    uint64_t output1,output2;
+    char c;
     if (myReadFile.is_open()) {
-        while (!myReadFile.eof()) {
-            myReadFile >> output1 >> output2;
+        while (myReadFile>>output1>>c>>output2 && c == ','){
             count++;
         }
         myReadFile.close();
@@ -32,17 +33,17 @@ int getArraysSize(string filename1,string filename2,uint64_t & count1,uint64_t &
     return 1;
 }
 
-int makeArray(string filename,mytuple * array1,uint64_t size){  //makes the array
+int makeArray(string filename,mytuple * array1){  //makes the array
     int c=0;
     ifstream myReadFile;
-    string output1,output2;
     myReadFile.open(filename);
-    size_t num;
+    uint64_t output1,output2;
+    char ch;
     if (myReadFile.is_open()) {
-        for(int i=0;i<size;i++){
-            myReadFile >> output1 >> output2;
+        while (myReadFile>>output1>>ch>>output2 && ch == ','){
             array1[c].index=c;
-            array1[c++].value = stoul(output1,&num);
+            array1[c].key = output1;
+            array1[c++].value = output2;
         }
         myReadFile.close();
         return 1;
@@ -50,12 +51,12 @@ int makeArray(string filename,mytuple * array1,uint64_t size){  //makes the arra
     return 0;
 }
 
-int makeArrays(string filename1,mytuple * array1,uint64_t count1,string filename2,mytuple * array2,uint64_t count2){
-    if(!makeArray(filename1,array1,count1)) {
+int makeArrays(string filename1,mytuple * array1,string filename2,mytuple * array2){
+    if(!makeArray(filename1,array1)) {
         cout<<"could not open file.\n";
         return 0;
     }
-    if(!makeArray(filename2,array2,count2)) {
+    if(!makeArray(filename2,array2)) {
         cout<<"could not open file.\n";
         return 0;
     }
